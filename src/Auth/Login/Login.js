9558aa15bffc18d5c8d7f7df3622fced6e433f6e
Janderson Martins <jandersonmartins10@gmail.com>
2020-06-16 22:03:58 -0300

    call setEnabled on start button

--- src/Auth/Login/Login.js
+++ src/Auth/Login/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Button, SafeAreaView} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {setStarted} from '../../store/ducks/app';
 import * as S from './Login.styles';
 
-const Login = () => (
-  <SafeAreaView>
-    <S.Container>
-      <Button title="Start" onPress={() => {}} />
-    </S.Container>
-  </SafeAreaView>
-);
+const Login = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <SafeAreaView>
+      <S.Container>
+        <Button title="Start" onPress={() => dispatch(setStarted(true))} />
+      </S.Container>
+    </SafeAreaView>
+  );
+};
 
 export default Login;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Text} from 'react-native';
 import {AuthRoutes} from '../Auth/AuthRoutes';
 
-const Routes = ({started}) => <AuthRoutes />;
+const Routes = ({started}) => (started ? <Text>Home</Text> : <AuthRoutes />);
 
 export default Routes;
